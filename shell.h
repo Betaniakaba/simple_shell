99694ac89db49863908bb19146835f33821eb9ee
Betania <Betania.berhanu@outlook.com>
2022-11-17 04:20:18 -0800

    Error Fix

--- shell.h
+++ shell.h
@@ -5,5 +5,11 @@ void env_shell(char **args, char *line, char **env);
 void exit_shell(char **args, char *line, char **env);
 void shell(int argc, char **argv, char **env);
 int main(int argc, char **argv, char **env);
+char *_getline(void);
+char **split_line(char *line);
+int check_for_builtings(char **args, char *line, char **env);
+int launch_prog(char **args);
+int  buitlins_checker(char **args);
+
 
 #endif
