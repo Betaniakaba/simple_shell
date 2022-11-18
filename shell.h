74a6a99d9109362875c607b381b3ddf29ca17332
Betania <Betania.berhanu@outlook.com>
2022-11-18 04:08:50 -0800

    Fix error

--- shell.h
+++ shell.h
@@ -12,4 +12,30 @@ int launch_prog(char **args);
 int  buitlins_checker(char **args);
 
 
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stddef.h>
+#include <errno.h>
+#include <sys/wait.h>
+#define TOKENS_BUFFER_SIZE 64
+#define LINE_SIZE 1024
+#define TOKEN_DELIMITERS " \t\r\n\a"
+extern char **environ;
+/**
+  * struct builtins - has builtins and associated funcs
+  * @arg: Builtins name
+  * @builtin: Matching builtin func
+  */
+typedef struct builtins
+{
+	char *arg;
+	void (*builtin)(char **args, char *line, char **env);
+}
+builtins_t;
+
 #endif
